c458de5944d4fa30c8c336d0a81dd24938c08b63
syaifulnizamyahya <syaifulnizamyahya@gmail.com>
2024-07-15 23:41:34 +0800

    Cleanup

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -32,23 +32,13 @@ void printVertices(const tinygltf::Model& model, int first) {
 
                 std::cout << "Mesh: " << mesh.name
                     << ", Primitive: " << primitive.material << std::endl;
-                if (first == 0) {
-                    for (size_t i = 0; i < vertexCount; ++i) {
-                        float x = positions[i * 3 + 0];
-                        float y = positions[i * 3 + 1];
-                        float z = positions[i * 3 + 2];
-                        std::cout << "Vertex " << i << ": (" << x << ", " << y << ", " << z
-                            << ")" << std::endl;
-                    }
-                }
-                else {
-                    for (size_t i = 0; i < first; ++i) {
-                        float x = positions[i * 3 + 0];
-                        float y = positions[i * 3 + 1];
-                        float z = positions[i * 3 + 2];
-                        std::cout << "Vertex " << i << ": (" << x << ", " << y << ", " << z
-                            << ")" << std::endl;
-                    }
+                int count = (first > 0) ? first : vertexCount;
+                for (size_t i = 0; i < count; ++i) {
+                    float x = positions[i * 3 + 0];
+                    float y = positions[i * 3 + 1];
+                    float z = positions[i * 3 + 2];
+                    std::cout << "Vertex " << i << ": (" << x << ", " << y << ", " << z
+                        << ")" << std::endl;
                 }
             }
         }
@@ -57,18 +47,8 @@ void printVertices(const tinygltf::Model& model, int first) {
 
 void printVertices(const tinygltf::Model& model) { printVertices(model, 0); }
 
-// Function to obfuscate vertex positions using XOR with a pseudo-random number
-// pros
-// 1. bitwise op is fast
-// cons
-// 1. totally deformed and blown object. unpredictable
-// 2. gltf metadata warning
+// Perform XOR to vertex component
 void obfuscateVertices(tinygltf::Model& model, unsigned int seed) {
-    // std::default_random_engine generator(seed);
-    // Using Mersen Twister random func. Should be platform agnostic (havent find
-    // any proof though). Alternatively use pcg.
-    // https://www.pcg-random.org/index.html Or use any deterministic, platform
-    // agnostic random method.
     std::mt19937 generator(seed);
     std::uniform_int_distribution<uint32_t> distribution;
 
@@ -81,35 +61,22 @@ void obfuscateVertices(tinygltf::Model& model, unsigned int seed) {
                     model.bufferViews[accessor.bufferView];
                 tinygltf::Buffer& buffer = model.buffers[bufferView.buffer];
 
-                // Casting to standard uint32, not float
                 uint32_t* positions = reinterpret_cast<uint32_t*>(
                     &buffer.data[bufferView.byteOffset + accessor.byteOffset]);
 
-                // not really vertex count. More like all vertex component (vertex count
-                // * 3)
-                size_t vertexCount = accessor.count * accessor.type;
+                size_t vertexComponentCount = accessor.count * accessor.type;
 
-                // FOR EACH VERTEX COMPONENT
-                for (size_t i = 0; i < vertexCount; ++i) {
-                    // if (i < 5) {
-                    //   std::cout << "Before pos:" << positions[i]
-                    //             << " val:" << std::bit_cast<float >(positions[i])
-                    //             << " rand:" << distribution(generator) << std::endl;
-                    // }
-                    // PERFORM XOR AGAINST RANDOM NUMBER
+                // For each vertex component (x, y, z)
+                for (size_t i = 0; i < vertexComponentCount; ++i) {
+                    // Perform XOR with random
                     positions[i] ^= distribution(generator);
-                    // if (i < 5) {
-                    //   std::cout << "After  pos:" << positions[i]
-                    //             << " val:" << std::bit_cast<float >(positions[i])
-                    //             << " rand:" << distribution(generator) << std::endl;
-                    // }
                 }
             }
         }
     }
 }
 
-// Vertex index random reordering
+// Shuffle vertices
 void shuffleVertices(tinygltf::Model& model, unsigned int seed) {
     std::mt19937 generator(seed);
 
@@ -126,33 +93,35 @@ void shuffleVertices(tinygltf::Model& model, unsigned int seed) {
                     &buffer.data[bufferView.byteOffset + accessor.byteOffset]);
 
                 size_t vertexCount = accessor.count;
-                // Store permutation of vertex index
+
+                // Storage for permutation of vertex index
                 std::vector<size_t> permutation(vertexCount);
+
                 // Fill with 0, 1, 2, ..., vertexCount-1. As correct index.
                 // permutation.at(1) = 1 etc
                 std::iota(permutation.begin(), permutation.end(), 0);
 
                 std::vector<float> tempPositions(vertexCount * 3);
-                // Suffle randomly the index based on random generator.
+
+                // Suffle vertex index based on random generator.
                 // permutation.at(1) = 224 etc
-                // std::shuffle(permutation.begin(), permutation.end(), generator);
                 std::ranges::shuffle(permutation.begin(), permutation.end(), generator);
 
-                // Copy the vertex component indexed in permutation to new vertex
-                // index(tempPositions)
+                // Swap index in permutation to new vertex
+                // vertex.at(1) = newVertex.at(224)
                 for (size_t i = 0; i < vertexCount; ++i) {
                     tempPositions[i * 3 + 0] = positions[permutation[i] * 3 + 0];
                     tempPositions[i * 3 + 1] = positions[permutation[i] * 3 + 1];
                     tempPositions[i * 3 + 2] = positions[permutation[i] * 3 + 2];
                 }
 
-                std::copy(tempPositions.begin(), tempPositions.end(), positions);
+                std::move(tempPositions.begin(), tempPositions.end(), positions);
             }
         }
     }
 }
 
-// Reverse the vertex shuffling using the stored permutation indices
+// Reverse the vertex shuffling 
 void reverseShuffleVertices(tinygltf::Model& model, unsigned int seed) {
     std::mt19937 generator(seed);
     for (auto& mesh : model.meshes) {
@@ -168,11 +137,16 @@ void reverseShuffleVertices(tinygltf::Model& model, unsigned int seed) {
                     &buffer.data[bufferView.byteOffset + accessor.byteOffset]);
 
                 size_t vertexCount = accessor.count;
-                std::vector<size_t> permutation(vertexCount);
-                std::iota(permutation.begin(), permutation.end(),
-                    0);  // Fill with 0, 1, 2, ..., vertexCount-1
 
-                // std::shuffle(permutation.begin(), permutation.end(), generator);
+                // Storage for permutation of vertex index
+                std::vector<size_t> permutation(vertexCount);
+
+                // Fill with 0, 1, 2, ..., 
+                // permutation.at(1) = 1 etc
+                std::iota(permutation.begin(), permutation.end(), 0);
+
+                // Suffle vertex index based on random generator.
+                // permutation.at(1) = 224 etc
                 std::ranges::shuffle(permutation.begin(), permutation.end(), generator);
 
                 std::vector<float> tempPositions(vertexCount * 3);
@@ -184,7 +158,7 @@ void reverseShuffleVertices(tinygltf::Model& model, unsigned int seed) {
                     tempPositions[permutation[i] * 3 + 2] = positions[i * 3 + 2];
                 }
 
-                std::copy(tempPositions.begin(), tempPositions.end(), positions);
+                std::move(tempPositions.begin(), tempPositions.end(), positions);
             }
         }
     }
@@ -211,32 +185,32 @@ int main(int argc, char** argv) {
     }
 
     tinygltf::Model model;
-    tinygltf::TinyGLTF gltf_ctx;
+    tinygltf::TinyGLTF reference;
     tinygltf::TinyGLTF encrypted;
     tinygltf::TinyGLTF decrypted;
 
     std::string err;
     std::string warn;
-    std::string input_filename(argv[1]);
+    std::string reference_filename(argv[1]);
     std::string encrypted_filename(argv[2]);
     std::string decrypted_filename(argv[3]);
-    std::string ext = GetFilePathExtension(input_filename);
+    std::string ext = GetFilePathExtension(reference_filename);
 
-    gltf_ctx.SetStoreOriginalJSONForExtrasAndExtensions(
+    reference.SetStoreOriginalJSONForExtrasAndExtensions(
         store_original_json_for_extras_and_extensions);
 
     bool ret = false;
     if (ext.compare("glb") == 0) {
         std::cout << "Reading binary glTF" << std::endl;
         // assume binary glTF.
-        ret = gltf_ctx.LoadBinaryFromFile(&model, &err, &warn,
-            input_filename.c_str());
+        ret = reference.LoadBinaryFromFile(&model, &err, &warn,
+            reference_filename.c_str());
     }
     else {
         std::cout << "Reading ASCII glTF" << std::endl;
         // assume ascii glTF.
         ret =
-            gltf_ctx.LoadASCIIFromFile(&model, &err, &warn, input_filename.c_str());
+            reference.LoadASCIIFromFile(&model, &err, &warn, reference_filename.c_str());
     }
 
     if (!warn.empty()) {
@@ -252,16 +226,20 @@ int main(int argc, char** argv) {
         return -1;
     }
 
+    // print original
     std::cout << "Original" << std::endl;
     printVertices(model, 5);
 
     int key = 1337;
     // ENCRYPT
-    obfuscateVertices(model, key);
-    // shuffleVertices(model, key);
+    //obfuscateVertices(model, key);
+    shuffleVertices(model, key);
+
+    //print encrypted
     std::cout << "Encrypted" << std::endl;
     printVertices(model, 5);
 
+    //write encrypted model
     ret = encrypted.WriteGltfSceneToFile(&model, encrypted_filename, true, true,
         true, false);
     if (!ret) {
@@ -269,16 +247,18 @@ int main(int argc, char** argv) {
             << std::endl;
         return -1;
     }
-
     std::cout << "Successfully encrypt and saved glTF file: "
         << encrypted_filename << std::endl;
 
     // DECRYPT
-    obfuscateVertices(model, key);
-    // reverseShuffleVertices(model, key);
+    //obfuscateVertices(model, key);
+    reverseShuffleVertices(model, key);
+
+    //print decrypted
     std::cout << "Decrypted" << std::endl;
     printVertices(model, 5);
 
+    //write decrypted model
     ret = decrypted.WriteGltfSceneToFile(&model, decrypted_filename, true, true,
         true, false);
     if (!ret) {
@@ -286,7 +266,6 @@ int main(int argc, char** argv) {
             << std::endl;
         return -1;
     }
-
     std::cout << "Successfully decrypt and saved glTF file: "
         << decrypted_filename << std::endl;
 
